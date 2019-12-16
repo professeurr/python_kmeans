59f7a9c6f830fd9497eb68328384e01e312fdaad
Kodjo <kodjo.klouvi@gmail.com>
2019-12-17 00:22:04 +0100

    first commit

--- kmeans-klouvi-riva.py
+++ kmeans-klouvi-riva.py
@@ -1,14 +1,16 @@
 from math import sqrt
 import random
 import sys
+import time
 
 from pyspark.sql import SparkSession
 
 
 LogBuffer = []
 
-def computeDistance(x,y):
-    return sqrt(sum([(a - b)**2 for a,b in zip(x,y)]))
+
+def computeDistance(x, y):
+    return sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))
 
 
 def closestCluster(dist_list):
@@ -18,68 +20,85 @@ def closestCluster(dist_list):
         if elem[1] < min_dist:
             cluster = elem[0]
             min_dist = elem[1]
-    return (cluster,min_dist)
+    return (cluster, min_dist)
 
-def sumList(x,y):
-    return [x[i]+y[i] for i in range(len(x))]
 
-def moyenneList(x,n):
-    return [x[i]/n for i in range(len(x))]
+def sumList(x, y):
+    return [x[i] + y[i] for i in range(len(x))]
+
+
+def meanList(x, n):
+    return [x[i] / n for i in range(len(x))]
+
+
+def split_line(line):
+    y = line.split(',')
+    return [float(x) for x in y[:-1]], y[-1]
 
 
 def log(x):
     LogBuffer.append(x)
 
 
-def logTitle(x):
-    log("============== {} ==============".format(x))
+debug = False
+def log_rdd(s, r):
+    if debug:
+        log('{}: {}'.format(s, r.collect()))
 
+
+def log_title(x):
+    log("============== {} ==============".format(x))
 
 def getLog():
     return LogBuffer
 
 
-def advancedKmeans(sc, data, nb_clusters, maxSteps):
+def advancedKmeans(sc, inputs, nb_clusters, maxSteps, seed):
     clusteringDone = False
-    number_of_steps = 0
-    current_error = float("inf")
+    number_of_steps = 1
+    prev_assignment, assignment = None, None
+    error = float("inf")
+
+    data = inputs.map(lambda x: (x[1], x[0][0])).persist()  # (0,Array(5.1, 3.5, 1.4, 0.2))
+    labels = inputs.map(lambda x: (x[1], x[0][1]))  # (0,Iris-setosa)
+
+    log_rdd('data', data)
+    log_rdd('labels', labels)
+
     # A broadcast value is sent to and saved  by each executor for further use
     # instead of being sent to each executor when needed.
     nb_elem = sc.broadcast(data.count())
 
     #############################
-    # Select initial centroides #
+    # Select initial centroids #
     #############################
 
-    centroides = sc.parallelize(data.takeSample('withoutReplacment',nb_clusters))\
-              .zipWithIndex()\
-              .map(lambda x: (x[1],x[0][1][:-1]))
+    centroids = sc.parallelize(data.takeSample('withoutReplacment', nb_clusters, seed)) \
+        .zipWithIndex() \
+        .map(lambda x: (x[1], x[0][1]))
+
+    log_rdd('centroids', centroids)
     # (0, [4.4, 3.0, 1.3, 0.2])
     # In the same manner, zipWithIndex gives an id to each cluster
 
     while not clusteringDone:
 
+        log_title("Step {}".format(number_of_steps))
         #############################
         # Assign points to clusters #
         #############################
 
-        joined = data.cartesian(centroides)
+        joined = data.cartesian(centroids)
         # ((0, [5.1, 3.5, 1.4, 0.2, 'Iris-setosa']), (0, [4.4, 3.0, 1.3, 0.2]))
 
         # We compute the distance between the points and each cluster
-        dist = joined.map(lambda x: (x[0][0],(x[1][0], computeDistance(x[0][1][:-1], x[1][1]))))
+        dist = joined.map(lambda x: (x[0][0], ((x[1][0], computeDistance(x[0][1], x[1][1])), x[0][1])))
         # (0, (0, 0.866025403784438))
-
-        dist_list = dist.groupByKey().mapValues(list)
-        # (0, [(0, 0.866025403784438), (1, 3.7), (2, 0.5385164807134504)])
-
-        # We keep only the closest cluster to each point.
-        min_dist = dist_list.mapValues(closestCluster)
-        # (0, (2, 0.5385164807134504))
+        log_rdd("dist", dist)
 
         # assignment will be our return value : It contains the datapoint,
         # the id of the closest cluster and the distance of the point to the centroid
-        assignment = min_dist.join(data)
+        assignment = dist.reduceByKey(lambda x, y: x if (x[0][1] < y[0][1]) else y)
 
         # (0, ((2, 0.5385164807134504), [5.1, 3.5, 1.4, 0.2, 'Iris-setosa']))
 
@@ -87,30 +106,30 @@ def advancedKmeans(sc, data, nb_clusters, maxSteps):
         # Compute the new centroid of each cluster #
         ############################################
 
-        clusters = assignment.map(lambda x: (x[1][0][0], x[1][1][:-1]))
+        clusters = assignment.map(lambda z: (z[1][0][0], (1, z[1][1], z[1][0][1])))
         # (2, [5.1, 3.5, 1.4, 0.2])
 
-        count = clusters.map(lambda x: (x[0],1)).reduceByKey(lambda x,y: x+y)
-        somme = clusters.reduceByKey(sumList)
-        centroidesCluster = somme.join(count).map(lambda x : (x[0],moyenneList(x[1][0],x[1][1])))
+        count = clusters.reduceByKey( lambda x, y: (x[0] + y[0], sumList(x[1], y[1]), x[2] + y[2]))
+
+        centroidesCluster = count.map(lambda x: (x[0], meanList(x[1][1], x[1][0])))
 
         ############################
         # Is the clustering over ? #
         ############################
 
         # Let's see how many points have switched clusters.
-        if number_of_steps > 0:
-            switch = prev_assignment.join(min_dist)\
-                                    .filter(lambda x: x[1][0][0] != x[1][1][0])\
-                                    .count()
+        if prev_assignment:
+            switch = assignment.join(prev_assignment).filter(lambda x: x[1][0][0] != x[1][1][0]).count()
         else:
-            switch = 150
+            switch = nb_elem.value
+        log("switch: {}".format(switch))
+
         if switch == 0 or number_of_steps == maxSteps:
             clusteringDone = True
-            error = sqrt(min_dist.map(lambda x: x[1][1]).reduce(lambda x,y: x + y))/nb_elem.value
+            error = sqrt(count.map(lambda x: x[1][2]).reduce(lambda x, y: x + y)) / nb_elem.value
         else:
-            centroides = centroidesCluster
-            prev_assignment = min_dist
+            centroids = centroidesCluster
+            prev_assignment = assignment
             number_of_steps += 1
 
     return (assignment, error, number_of_steps)
@@ -120,18 +139,21 @@ if __name__ == "__main__":
 
     spark = SparkSession.builder.appName('KMeans_Python_Klouvi_Riva_{}'.format(random.randint(0, 1000000))).getOrCreate()
     sc = spark.sparkContext
+    sc.setLogLevel('ERROR')
+
     input_file = sys.argv[1]
+    max_partitions = 1
+    nb_clusters = 3
+    max_steps = 100
+    seed = 42
+    start_time = time.time()
 
     lines = sc.textFile(input_file)
-    data = lines.map(lambda x: x.split(','))\
-            .map(lambda x: [float(i) for i in x[:4]]+[x[4]])\
-            .zipWithIndex()\
-            .map(lambda x: (x[1],x[0]))
-    # zipWithIndex allows us to give a specific index to each point
-    # (0, [5.1, 3.5, 1.4, 0.2, 'Iris-setosa'])
+    data = lines.map(lambda x: split_line(x)).zipWithIndex()
 
-    clustering = advancedKmeans(sc, data, 3, 1)
+    clustering = advancedKmeans(sc, data, nb_clusters, max_partitions, seed)
 
+    duration = (time.time() - start_time)
     outputPath = "{}/../kmeans_python_cluster".format(input_file)
     metricsPath = "{}/../kmeans_python_metrics".format(input_file)
 
@@ -139,6 +161,7 @@ if __name__ == "__main__":
     log("metrics path: {}".format(metricsPath))
     log("error: {}".format(clustering[1]))
     log("number of steps: {}".format(clustering[2]))
+    log("duration: {} s".format(duration))
 
     clustering[0].coalesce(1).saveAsTextFile(outputPath)
 
@@ -146,4 +169,3 @@ if __name__ == "__main__":
     metrics.coalesce(1).saveAsTextFile(metricsPath)
 
     print(clustering)
-
